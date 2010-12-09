f2c761d564c1c4c3887e3e89de7c07e28d6a85a6
SIMOND François <francois@lienweb.fr>
2010-12-09 15:00:33 +0100

    change how you call the script with better options management, some refactoring and changed error return codes

--- editor.sh
+++ editor.sh
@@ -14,12 +14,24 @@ done
 
 if [ $COMPILER-gcc > /dev/null = 127 ]  ; then
 	echo "****** You must let me know where the compiler is! ******"
-	exit
+	exit 1
 fi
 
+
+# parse command line options
+while getopts "s:d:r:c:" opt
+do
+	case "$opt" in
+		s) source_zImage=`readlink -f "$OPTARG"`;;
+		d) dest_zImage=`readlink -f "$OPTARG"`;;
+		r) new_ramdisk=`readlink -f "$OPTARG"`;;
+		c) compression="$OPTARG";;
+	esac
+done
+
+
+
 cur_space=$PWD
-zImage=$1
-new_ramdisk=$2
 determiner=0
 Image_here="./out/Image"
 
@@ -30,56 +42,63 @@ else
 fi
 
 echo "##### My name is $0 #####"
-if [ "$1" = "" ]; then
-	echo "****** You must let me know where the zImage file is ******"
-	exit
+if [ "$source_zImage" = "" ]; then
+	echo "****** You must let me know where the source zImage file is ******"
+	exit 1
 else
-	echo "##### The kernel is $1 #####"
+	echo "##### The source kernel is $source_zImage #####"
 fi
 
-if [ "$2" = "" ]; then
+if [ "$dest_zImage" = "" ]; then
+	echo "****** You must let choose a destination file for the resulting zImage ******"
+	exit 1
+else
+	echo "##### The destination zImage file is $dest_zImage #####"
+fi
+
+if [ "$new_ramdisk" = "" ]; then
 	echo "****** You must let me know where the ramdisk directory is ******"
-	exit
-elif [ -d $2 ] ; then
+	exit 1
+elif [ -d $new_ramdisk ] ; then
 	echo "##### The directory of the ramdisk has been selected #####"
-elif [ -f $2 ] ; then
+elif [ -f $new_ramdisk ] ; then
 	echo "##### The compressed file of the ramdisk has been selected #####"
 	ramdisk_type="file"
 fi
 
-if [ "$3" = "gz" ]; then
-	echo "##### The ramdisk is $2 (will be gzipped) #####"
-	cd $2
+if [ "$compression" = "gz" ]; then
+	echo "##### The ramdisk is $new_ramdisk (will be gzipped) #####"
+	cd $new_ramdisk
 	find . -print0 | cpio -o0 -H newc | gzip -9 -f > $cur_space/out/initramfs_data.cpio.gz
 	new_ramdisk=out/initramfs_data.cpio.gz
 	cd $cur_space
-elif [  "$3" = "lzma" ]; then
-	echo "##### The ramdisk is $2 (will be lzma'ed) #####"
-	/bin/bash resources/2.6.29/scripts/gen_initramfs_list.sh -o out/initramfs_data.cpio.lzma  -u "squash"  -g "squash"  $2
+elif [  "$compression" = "lzma" ]; then
+	echo "##### The ramdisk is $new_ramdisk (will be lzma'ed) #####"
+	bash resources/2.6.29/scripts/gen_initramfs_list.sh -o out/initramfs_data.cpio.lzma  -u "squash"  -g "squash"  $new_ramdisk
 	new_ramdisk=out/initramfs_data.cpio.lzma
-elif [  "$3" = "" ] ; then
+elif [  "$compression" = "" ] ; then
 	if [ "$ramdisk_type" = "file" ] ; then
-		echo "##### The ramdisk is $2 (already compressed) #####"
-		new_ramdisk=$2
+		echo "##### The ramdisk is $new_ramdisk (already compressed) #####"
+		new_ramdisk=$new_ramdisk
 	else
 		echo "****** You must let me know how you want to compress the ramdisk's directory ******"
-		exit
+		exit 1
 	fi
 fi
 
-#=======================================================
-# find start of gziped kernel object in the zImage file:
-#=======================================================
+#==============================================================
+# find start of gziped kernel object in the source zImage file:
+#==============================================================
 
-pos=`grep -P -a -b --only-matching '\x1F\x8B\x08' $zImage | cut -f 1 -d : | grep '1' | awk '(NR==1)'`
+pos=`grep -P -a -b --only-matching '\x1F\x8B\x08' $source_zImage | cut -f 1 -d : | grep '1' | awk '(NR==1)'`
 echo "##### 01.  Extracting kernel  from $zImage (start = $pos)"
-dd if=$zImage bs=1 skip=$pos | gunzip > $Image_here
+dd if=$source_zImage bs=1 skip=$pos | gunzip > $Image_here
 
-#===========================================================================
+#==========================================================================
 # find start and end of the "cpio" initramfs  inside the kernel object:
 # ASCII cpio header starts with '070701'
 # The end of the cpio archive is marked with an empty file named TRAILER!!!
-#===========================================================================
+#==========================================================================
 start=`grep -a -b --only-matching '070701' $Image_here | head -1 | cut -f 1 -d :`
 end=`grep -a -b --only-matching 'TRAILER!!!' $Image_here | head -1 | cut -f 1 -d :`
 
@@ -88,7 +107,7 @@ count=$((end - start))
 
 if [ $count -lt $determiner ]; then
 	echo "##### ERROR : Couldn't match start/end of the initramfs ."
-	exit
+	exit 2
 fi
 
 # Check the new ramdisk's size
@@ -96,7 +115,7 @@ ramdsize=`ls -l $new_ramdisk | awk '{print $5}'`
 echo "##### 02. The size of the new ramdisk is = $ramdsize / original = $count"
 if [ $ramdsize -gt $count ]; then
 	echo "****** Your initramfs needs to be smaller than the present!! ******"
-	exit
+	exit 2
 else
 	mv $new_ramdisk out/ramdisk.cpio
 fi
@@ -129,7 +148,7 @@ if [ $franksize -lt $end ]; then
 	cat out/franken.img out/newtail.img > out/new_Image
 else
 	echo "##### ERROR : Your initramfs is still BIGGER than the stock initramfs #####"
-	exit
+	exit 3
 fi
 
 #============================================
@@ -168,8 +187,9 @@ $COMPILER-objcopy -O binary -R .note -R .note.gnu.build-id -R .comment -S  arch/
 
 # finishing
 echo "##### 15. Getting finished!!"
-cp -f arch/arm/boot/zImage ../../new_zImage
+cp -f arch/arm/boot/zImage $dest_zImage
 rm -r arch/arm/boot/compressed/vmlinux arch/arm/boot/compressed/piggy.o arch/arm/boot/compressed/misc.o arch/arm/boot/compressed/head.o arch/arm/boot/compressed/piggy.gz arch/arm/boot/Image arch/arm/boot/zImage ../../out
+
 
 echo "#=========================================="
 echo "##### DONE!! #####"
